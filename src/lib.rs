e216570c0104fc46f8a632073c2090e1e1f0c090
sudo-rushil <rushil.mallarapu@gmail.com>
2020-04-25 14:17:04 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub fn add() -> u32 {
+    2
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ tests/seventh-core.rs
@@ -0,0 +1,6 @@
+use seventh_core::add;
+
+#[test]
+fn add_is_two() {
+    assert_eq!(add(), 2);
+}
