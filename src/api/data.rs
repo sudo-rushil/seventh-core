97d18ce06123d4cef7572446025aff9396974be0
sudo-rushil <rushil.mallarapu@gmail.com>
2020-04-26 20:40:26 -0400

    implemented v1 data retrieval functionality

--- src/api/data.rs
+++ src/api/data.rs
@@ -1,12 +1,17 @@
 //! Retrieve data from Coinbase and CoinGecko APIs
 
 use std::fmt;
+use chrono::Utc;
+use phf::{Map, phf_map};
 
-/// Response types
+const UNIX_DAY: i64 = 86400;
 
-/// endpoints:
-/// coin/marketchart/range
-/// prices - buy, sell, spot
+static COIN_ID: Map<&'static str, &'static str> = phf_map! {
+    "BTC" => "bitcoin",
+    "ETH" => "ethereum",
+    "ETC" => "ethereum-classic",
+    "LTC" => "litecoin",
+};
 
 #[derive(Deserialize, Debug)]
 struct Price {
@@ -26,12 +31,36 @@ pub struct HistoricalData {
     total_volumes: Vec<Vec<f32>>
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct CoinData {
     ticker: String,
     historical: Vec<f32>,
     buy: f32,
     sell: f32,
     spot: f32,
+}
+
+impl CoinData {
+    fn new(buyprice: Price, sellprice: Price, spotprice: Price, history: HistoricalData) -> Self {
+        CoinData {
+            ticker: spotprice.base,
+            historical: history.prices.into_iter().map(|v| v[1]).collect(),
+            buy: buyprice.amount.parse().unwrap(),
+            sell: sellprice.amount.parse().unwrap(),
+            spot: spotprice.amount.parse().unwrap()
+        }
+    }
+}
+
+impl fmt::Display for CoinData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CoinData: ticker {}, buy {}, sell {}, spot {}",
+            self.ticker,
+            self.buy,
+            self.sell,
+            self.spot
+        )
+    }
 }
 
 pub struct DataAPI {
@@ -41,20 +70,51 @@ pub struct DataAPI {
 
 impl DataAPI {
     #[tokio::main]
-    async fn get<T: for<'de> serde::Deserialize<'de>>(&self, endpoint: &str) -> Result<T, reqwest::Error> {
-        let response = self.client.get(endpoint).send().await?;
+    async fn get<T: for<'de> serde::Deserialize<'de>>(&self, endpoint: String) -> Result<T, reqwest::Error> {
+        let response = self.client.get(&endpoint).send().await?;
         let result: T = response.json().await?;
 
         Ok(result)
     }
 
-    pub fn new() -> Self {
-        DataAPI { client: reqwest::Client::new(), coins: vec![CoinData { ticker: "BTC".to_owned(), historical: vec![2.0], buy: 2.0, sell: 2.0, spot: 2.0 }] }
-    }
-}
+    fn get_coinbase(&self, coin: &str, query: &str) -> Price {
+        let endpoint = format!("https://api.coinbase.com/v2/prices/{}-USD/{}", coin, query);
+        let result = self.get::<PriceData>(endpoint.clone());
 
-impl fmt::Display for CoinData {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "DataAPI: buy {}, sell {}, spot {}", self.buy, self.sell, self.spot)
+        match result {
+            Ok(price_data) => price_data.data,
+            Err(e) => panic!("Error calling Coinbase API: {}", e)
+        }
     }
+
+    fn get_historical(&self, coin: &str) -> HistoricalData {
+        let curr_time = Utc::now().timestamp();
+        let endpoint = format!("https://api.coingecko.com/api/v3/coins/{}/market_chart/range?vs_currency=usd&from={}&to={}", coin, curr_time - UNIX_DAY, curr_time);
+        let result = self.get::<HistoricalData>(endpoint);
+
+        match result {
+            Ok(historical_data) => historical_data,
+            Err(e) => panic!("Error calling CoinGecko API: {}", e)
+        }
+    }
+
+    pub fn update(&mut self, coin: &str) {
+        let buy = self.get_coinbase(coin, "buy");
+        let sell = self.get_coinbase(coin, "sell");
+        let spot = self.get_coinbase(coin, "spot");
+        let history = self.get_historical(COIN_ID[coin]);
+
+        let new_data = CoinData::new(buy, sell, spot, history);
+
+        self.coins.push(new_data)
+    }
+
+    pub fn new() -> Self {
+        DataAPI { client: reqwest::Client::new(), coins: vec![] }
+    }
+
+    pub fn coins(&self) -> Vec<CoinData> {
+        self.coins.clone()
+    }
+
 }
--- tests/api.rs
+++ tests/api.rs
@@ -1,7 +1,15 @@
 use seventh_core::api::data::*;
 
 #[test]
-fn get_response() {
-    DataAPI::new().get::<PriceData>("https://api.coinbase.com/v2/prices/BTC-USD/spot");
-    assert!(true);
+fn test_initialization() {
+    let data_api = DataAPI::new();
+    assert_eq!(data_api.coins(), vec![]);
+}
+
+#[test]
+fn test_update() {
+    let mut data_api = DataAPI::new();
+    assert_eq!(data_api.coins(), vec![]);
+    data_api.update("BTC");
+    assert_eq!(data_api.coins().len(), 1);
 }
